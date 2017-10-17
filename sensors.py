cc77abaf6a4c73a9ab6255ff8c95a9d7f796978c
Gareth <garethjsharpe@gmail.com>
2017-10-16 21:46:09 -0400

    Setup PiPlates DAQC prototypes

--- sensors.py
+++ sensors.py
@@ -5,30 +5,43 @@ Created on Oct 16, 2017
 '''
 
 # import RPi.GPIO as GPIO
+# import piplates.DAQCplate as DAQC 
 
 from datetime import datetime
 
-# define number of sensors constant
+# define sensor constants
 NUM_SENSORS = 5
 SENSORS = ["Temperature", "Humidity", "Light", "Pressure", "Flow"]
 MAX_ERROR_VALUES = [10, 20, 30, 40, 50]
 MIN_ERROR_VALUES = [0, 0, 0, 0, 0]
 
-# define GPIO pins with variables
-read_pin = 18
-s1_pin = 0
-s2_pin = 1
-s3_pin = 3
-s4_pin = 4
-s5_pin = 5
+# define piplate address
+ADDRESS = 0
+
+# define red and green colours
+RED = 0
+GREEN = 1
+
+# ANALOG TO DIGITAL FUNCTIONS
+# DAQC.getADC(ADDRESS, channel) - return voltage from single channel
+# DAQC.getADCall(ADDRESS) - return voltage from all channels 
+
+# TODO: implement green status light
+# green led signifies run
+# DAQC.setLED(ADDRESS, color)
 
 def get_sensor_data():
-    s1_data = 1 # analog_read(s1_pin)
-    s2_data = 2 # analog_read(s2_pin)
-    s3_data = 3 # analog_read(s3_pin)
-    s4_data = 4 # analog_read(s4_pin)
-    s5_data = 5 # analog_read(s5_pin)
     timestamp = str(datetime.now())
+    # TODO: remove testing data and call individual functions
+    # TESTING
+    s1_data = 1 # DAQC.getADC(ADDRESS, 0)
+    s2_data = 2 # DAQC.getADC(ADDRESS, 1)
+    s3_data = 3 # DAQC.getADC(ADDRESS, 2)
+    s4_data = 4 # DAQC.getADC(ADDRESS, 3)
+    s5_data = 5 # DAQC.getADC(ADDRESS, 4)
+
+    # TODO: implement formatting functions
+    # temp = round((temp_data - 2.73) * 100, 2)
     return [timestamp, s1_data, s2_data, s3_data, s4_data, s5_data]
 
 # create discharge function for reading capacitor data
@@ -53,5 +66,7 @@ def is_error(data):
             errors_data.append(data[sensor])
             errors_sensors.append(SENSORS[sensor])
     return error_data, error_sensors
+    
+
 
 	
